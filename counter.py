4b336412430a661cfd17ff7986d944dd0bf5a863
Makhutso Poto <makhutsopotoofficial@gmail.com>
2026-05-16 23:10:48 +0200

    Add static method to reset instance count

--- counter.py
+++ counter.py
@@ -3,5 +3,6 @@ class InstanceCounter():
     def __init__(self):
             InstanceCounter.count +=1
             self.id =InstanceCounter.count 
+    @staticmethod
     def reset():
         InstanceCounter.count = 0 
